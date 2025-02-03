5ebb750b68360368ae45ab58acf991889d9819bb
Sarah-Kemppainen <sarahjkemppainen@gmail.com>
2025-02-03 15:25:19 -0600

    controls LEDs to flash when switch is pressed

---
+++ Project_1.ino
@@ -0,0 +1,23 @@
+void setup() {
+  // initialize pins and ports
+  // configure switch (port 33-35) 
+
+  // configure LEDS (ports 20-17)
+
+  // initialize new_pattern:: the flashing pattern for LEDS
+
+
+
+}
+
+void loop() {
+  // enter condition if switch is pressed("if (pressed == true) { }")
+
+    // start pattern (GPIOx_PDOR = (GPIOx_PDOR & ~mask) | new_pattern;)
+
+    
+  
+
+
+
+}
