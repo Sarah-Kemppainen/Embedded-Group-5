26c61054523cf7fb191e4f18793b7656fc7eb52f
Sarah-Kemppainen <sarahjkemppainen@gmail.com>
2025-02-04 12:46:45 -0600

    flashes LEDs for ports 32-29; troubleshooted

--- Project_1.ino
+++ Project_1.ino
@@ -1,38 +1,37 @@
+#define LED_PIN1 32 // define the LED pin
+#define LED_PIN2 31
+#define LED_PIN3 30
+#define LED_PIN4 29
+
 void setup() {
-
   // Configure LED ports
-  int led1 = 17;
-  int led2 = 18;
-  int led3 = 19;
-  int led4 = 20;
-
-  pinMode(led1, OUTPUT);
-  pinMode(led2, OUTPUT);
-  pinMode(led3, OUTPUT);
-  pinMode(led4, OUTPUT);
-
-  // configure switch (port 33-35) 
-
-  // configure LEDS (ports 20-17)
-
+  pinMode(LED_PIN1, OUTPUT);// set pin 32 as an ouput
+  pinMode(LED_PIN2, OUTPUT);
+  pinMode(LED_PIN3, OUTPUT);
+  pinMode(LED_PIN4, OUTPUT);
 }
 
 void loop() {
-  // enter condition if switch is pressed("if (pressed == true) { }")
 
-  // start pattern (GPIOx_PDOR = (GPIOx_PDOR & ~mask) | new_pattern;)
-
-  analogWrite(led1, 200);
+  analogWrite(LED_PIN1, 200);
   delay(500);
-  analogWrite(led2, 200);
+  analogWrite(LED_PIN1, 0);
   delay(500);
-  analogWrite(led3, 200);
+
+  analogWrite(LED_PIN2, 200);
   delay(500);
-  analogWrite(led4, 200);
+  analogWrite(LED_PIN2, 0);
+  delay(500);
 
-    
-  
+  analogWrite(LED_PIN3, 200);
+  delay(500);
+  analogWrite(LED_PIN3, 0);
+  delay(500);
 
-
+  analogWrite(LED_PIN4, 200);
+  delay(500);
+  analogWrite(LED_PIN4, 0);
+  delay(500);
+ 
 
 }
