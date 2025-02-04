b65edb6c7654d78e8c5290b20b8a87872ad7233e
Sarah-Kemppainen <sarahjkemppainen@gmail.com>
2025-02-04 10:00:40 -0600

    initializes ports and controls LEDS

--- Project_1.ino
+++ Project_1.ino
@@ -1,19 +1,34 @@
 void setup() {
-  // initialize pins and ports
+
+  // Configure LED ports
+  int led1 = 17;
+  int led2 = 18;
+  int led3 = 19;
+  int led4 = 20;
+
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
+
   // configure switch (port 33-35) 
 
   // configure LEDS (ports 20-17)
-
-  // initialize new_pattern:: the flashing pattern for LEDS
-
-
 
 }
 
 void loop() {
   // enter condition if switch is pressed("if (pressed == true) { }")
 
-    // start pattern (GPIOx_PDOR = (GPIOx_PDOR & ~mask) | new_pattern;)
+  // start pattern (GPIOx_PDOR = (GPIOx_PDOR & ~mask) | new_pattern;)
+
+  analogWrite(led1, 200);
+  delay(500);
+  analogWrite(led2, 200);
+  delay(500);
+  analogWrite(led3, 200);
+  delay(500);
+  analogWrite(led4, 200);
 
     
   
