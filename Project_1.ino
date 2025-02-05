1eaa9cab3268d31290e7c385babf889830e89f9d
Sarah-Kemppainen <sarahjkemppainen@gmail.com>
2025-02-04 21:27:56 -0600

    defines LEDs with GPIO direct register access

--- Project_1.ino
+++ Project_1.ino
@@ -1,56 +1,60 @@
-// define LED pin ports (output)
-#define LED_PIN1 32
-#define LED_PIN2 31
-#define LED_PIN3 30
-#define LED_PIN4 29
-
-// define switch port (input)
-#define SwitchIn A0
-
-int switchState = 0;  // stores if switch is in the true or false position
+// Define LED pin (output)
+#define SWITCH_PIN 25
 
 void setup() {
-  // Configure LED ports
-  pinMode(LED_PIN1, OUTPUT);  // set pin 32 as an ouput
-  pinMode(LED_PIN2, OUTPUT);  // set pin 31 as an output
-  pinMode(LED_PIN3, OUTPUT);  // set pin 30 as an output
-  pinMode(LED_PIN4, OUTPUT);  // set pin 29 as an output
+    PORTC_PCR5 = PORT_PCR_MUX(0x1);
+    PORTD_PCR5 = PORT_PCR_MUX(0x1);
+    PORTA_PCR5 = PORT_PCR_MUX(0x1);
+    PORTB_PCR11 = PORT_PCR_MUX(0x1);
+    PORTE_PCR25 = PORT_PCR_MUX(0x0);
 
-  // Configure switch port
-  pinMode(SwitchIn, INPUT); // set port () as an input
+    // Configure the LED port as output
+    GPIOC_PDDR |= 0b100000;
+    GPIOD_PDDR |= 0b100000;
+    GPIOA_PDDR |= 0b100000;
+    GPIOB_PDDR |= 0b100000000000;
+   
 
-} // void setup()
+   GPIOE_PDDR &= ~(1 << SWITCH_PIN); // Set PTE25 as input
+
+}
 
 void loop() {
-  // read switch state
-  switchState = digitalRead(SwitchIn);
-
-  // enter pattern if switch state is true
-  if (switchState == HIGH) {
-
-    // turn pin1 on and off
-    analogWrite(LED_PIN1, 200); // turn pin1 on
-    delay(500);                 // wait 500 ms
-    analogWrite(LED_PIN1, 0);   // turn pin1 off
-    delay(500);                 // wait 500 ms
-
-    // turn pin2 on and off
-    analogWrite(LED_PIN2, 200);
+    // The loop can be empty; if you want the LED to stay on, nothing else is needed.
+    
+    // Turn on LED.
+    GPIOC_PDOR |= 0b100000;
     delay(500);
-    analogWrite(LED_PIN2, 0);
+    GPIOC_PDOR ^= 0b100000;
     delay(500);
+     GPIOB_PDOR |= 0b100000000000;
+    delay(500);
+    GPIOB_PDOR ^= 0b100000000000;
+    delay(500);
+     GPIOA_PDOR |= 0b100000;
+    delay(300);
+    GPIOA_PDOR ^= 0b100000;
+    delay(300);
+    GPIOD_PDOR |= 0b100000;
+    delay(300);
+    GPIOD_PDOR ^= 0b100000;
+    delay(300);
 
-    // turn pin3 on and off
-    analogWrite(LED_PIN3, 200);
-    delay(500);
-    analogWrite(LED_PIN3, 0);
-    delay(500);
+    if (GPIOE_PDIR & (1 << SWITCH_PIN)) {
+        // Switch open
+        GPIOC_PDOR = 0x0;// Turn off LED PTC5
+        GPIOB_PDOR = 0x0;
+        GPIOA_PDOR = 0x0;
+        GPIOD_PDOR = 0x0;
+    } else {
+        // Switch closed
+        GPIOC_PDOR |= 0b100000;// Turn on LED PTC5
+        GPIOA_PDOR |= 0b100000;
+        GPIOB_PDOR |= 0b100000;
+        GPIOD_PDOR |= 0b100000000000;
+    }
+   
+}
 
-    // turn pin4 on and off
-    analogWrite(LED_PIN4, 200);
-    delay(500);
-    analogWrite(LED_PIN4, 0);
-    delay(500);
 
-  } // if (switchState == HIGH)
-} // void loop()
+
