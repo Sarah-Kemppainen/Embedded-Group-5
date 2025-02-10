ed9ed337116bf74f4b1dcc5493d07a4b495697fd
Sarah-Kemppainen <sarahjkemppainen@gmail.com>
2025-02-10 14:02:56 -0600

    talks to graph bar and turns on LEDs by mm input

--- Project_2.ino
+++ Project_2.ino
@@ -1,14 +1,46 @@
+void display_distance(float dist);
+
 void setup() {
   Serial.begin(115200);
   Serial.setTimeout(100000000);
   delay(1000);
 
+  // setup graph bar
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  //pinMode(8, OUTPUT);
+
+}
+
+void display_distance(float dist) {
+  // write display_distance function here
+  //dist_in_meters = dist / 1000
+
+  int pins_turned_on = dist / 100;
+
+  for (int i = 2; i <= pins_turned_on; i++) {
+    Serial.printf("Turning on pin %d\n", i);
+    digitalWrite(i, HIGH);
+  }
+  for (int i = 7; i > pins_turned_on; i--) {
+    Serial.printf("Turning off pin %d\n", i+1);
+    digitalWrite(i, LOW);
+  }
+
+
 }
 
 void loop() {
-  \\ Prompt the user for input.
-  Serial.printf("Enter the distance.\n");
-  \\ Read the value.
+  // Prompt the user for input.
+  Serial.printf("Enter the distance (in milimeters) within the range [100 mm, 800 mm]:\n");
+  // Read the value.
   int i = Serial.parseInt();
+
+
+  display_distance(i);
 
 }
