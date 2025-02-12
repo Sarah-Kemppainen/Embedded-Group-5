33a0e85e3adcf881141b65bf5e1d7dc7d1b5e5f4
Sarah-Kemppainen <sarahjkemppainen@gmail.com>
2025-02-12 14:30:42 -0600

    combined both if-statements into a single if-else

--- Project_2.ino
+++ Project_2.ino
@@ -21,7 +21,23 @@ void display_distance(float dist) {
   //dist_in_meters = dist / 1000
 
   int pins_turned_on = dist / 100;
+  Serial.printf("%d:\n", pins_turned_on);
 
+  int pin_states[5];
+  for (int i = 0; i < 5; i++) {
+    if (i <= pins_turned_on){
+      pin_states[i] = 1;
+      digitalWrite(i, HIGH);
+    } else {
+      pin_states[i] = 0;
+      digitalWrite(i, LOW);
+    }
+    Serial.printf("%d ", pin_states[i]);
+  }
+  Serial.printf("\n");
+
+
+/*
   for (int i = 2; i <= pins_turned_on; i++) {
     Serial.printf("Turning on pin %d\n", i);
     digitalWrite(i, HIGH);
@@ -30,11 +46,12 @@ void display_distance(float dist) {
     Serial.printf("Turning off pin %d\n", i+1);
     digitalWrite(i, LOW);
   }
-
+*/
 
 }
 
 void loop() {
+  Serial.printf("\n\n***** NEW RUN *****\n");
   // Prompt the user for input.
   Serial.printf("Enter the distance (in milimeters) within the range [100 mm, 800 mm]:\n");
   // Read the value.
